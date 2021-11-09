bc72f532750ea24736d8090d05c08aabded89099
areejsmac <joo123816@gmail.com>
2021-11-09 18:06:36 +0300

    navigation

--- app/src/main/java/com/example/noteappdb/MainActivity.kt
+++ app/src/main/java/com/example/noteappdb/MainActivity.kt
@@ -31,7 +31,6 @@ class MainActivity : AppCompatActivity() {
     private lateinit var rv: RecyclerView
     private lateinit var rvAdapter :RVA
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
